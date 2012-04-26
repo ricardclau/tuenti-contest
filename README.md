edc195fe07cf7b4045454d32bb19b87cbb7a6bf2
Ricard Clau <ricard.clau@gmail.com>
2012-04-26 19:00:20 +0200

    delete files

--- README.md
+++
@@ -1,4 +0,0 @@
-Soluciones al Tuenti Contest
-============================
-
-Me quedé en el 16... con un poco más de tiempo y inspiración quién sabe! :)
--- test1.php
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/php
-<?php
-
-$lines = file('php://stdin');
-foreach($lines as $line) {
-    echo array_reduce(preg_split('/\s/', trim($line)), 'bcadd') .  PHP_EOL;
-}
